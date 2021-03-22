34e6dda36a77518d9b23fd7d725eecea9ec9c990
Charles CEBE <charles.cebe@fxinnovation.com>
2021-03-22 17:56:19 -0400

    feat: add reusable locals for module
    add reusable locals to reuse across the module:
    - log_group_path: will concatenate the prefix with log_group_name.
    - log_kms_key_alias_name: will concatenate the prefix and the
      log_kms_key_name.
    - log_kms_key_id: will conditionally use the kms key datasource or the
      created resource depending if we set log_kms_key_create input variable
      to true or false.
    - default tags: will merge tags input variable with all essential and mandatory default tags to respect the standards (managed-by = terraform and Terraform = true).

---
+++ locals.tf
@@ -0,0 +1,30 @@
+#####
+# Locals
+#####
+
+locals {
+  log_group_path = format("%s/%s",
+    var.prefix,
+    var.log_group_name
+  )
+
+  log_kms_key_alias_name = format(
+    "alias/%s/%s",
+    var.prefix,
+    var.log_kms_key_name
+  )
+
+  log_kms_key_id = (
+    var.log_kms_key_create ?
+    element(concat(aws_kms_key.this.*.arn, [""]), 0) :
+    element(concat(data.aws_kms_key.this.*.arn, [""]), 0)
+  )
+
+  tags = merge(
+    {
+      managed-by = "terraform"
+      Terraform  = true
+    },
+    var.tags
+  )
+}
